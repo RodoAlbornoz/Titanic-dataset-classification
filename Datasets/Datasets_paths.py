5c355759624bd0e0b8d303f72221e3b9126d577c
RodoAlbornoz <ralbornozt@fi.uba.ar>
2023-09-26 15:10:27 -0300

    Making some changes

---
+++ Datasets/Datasets_paths.py
@@ -0,0 +1,2 @@
+ds_train_path = r"C:\Users\Administrador\Documents\IA\Proyectos\Titanic\Datasets\train.csv"
+ds_test_path = r"C:\Users\Administrador\Documents\IA\Proyectos\Titanic\Datasets\test.csv"
